b8f4f62b72bab720ae410b17dfedf5e82ab0bce4
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2026-03-18 19:50:25 +0100

    Console log tests

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 import {fibs, fibsRec} from "./fibonacci.js";
+import  {mergeSort} from "./mergeSort.js";
 
-//Test Fibonacci
+
+console.log("=== FIBONACCI TESTS ===");
 console.log(fibs(8));
 console.log(fibsRec(8));
+
+console.log("\n=== MERGE SORT TESTS ===");
+console.log(mergeSort([]));
+console.log(mergeSort([73]));
+console.log(mergeSort([1, 2, 3, 4, 5]));             
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+console.log(mergeSort([105, 79, 100, 110]));
